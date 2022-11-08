cc748bd65115f8ade117eed4b5e4d73f01591eea
Jose Freddy <dev.fherrera@gmail.com>
2022-11-07 23:22:13 -0600

    funcional practica del ngIf y ngFor con objetos

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -12,4 +12,51 @@ export class BodyComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  mostrar=true;
+
+  contactos:any={
+    mensaje:"",
+    autor:""
+  };
+
+  vehiculos = [
+    {
+      "img":"../../../assets/toyota.png",
+      "marca":"Toyota",
+      "modelo":"Hilux",
+      "anio":2020,
+      "precio":"$37,000"
+    },
+    {
+      "img":"../../../assets/nissan-ft.jpg",
+      "marca":"Nissan",
+      "modelo":"Frontier",
+      "anio":2021,
+      "precio":"$27,000"
+    },
+    {
+      "img":"../../../assets/ram.jpg",
+      "marca":"Dodge",
+      "modelo":"RAM",
+      "anio":2022,
+      "precio":"$57,000"
+    },
+    {
+      "img":"../../../assets/l200.png",
+      "marca":"Mitsubishi",
+      "modelo":"L200",
+      "anio":2022,
+      "precio":"$47,000"
+    },
+    {
+      "img":"../../../assets/chevrolet.png",
+      "marca":"Chevrolet",
+      "modelo":"Silverado ",
+      "anio":2022,
+      "precio":"$55,000"
+    }
+];
+
 }
+
+  
